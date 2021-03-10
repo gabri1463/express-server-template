a800ac49f7cb3a1f81695262b528aa48e2ff554f
Lucas Gabrielson <>
2021-03-10 13:24:56 -0600

    finished templates

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
 // requires
+const express = require( 'express' );
+const app = express();
 
-// 
+// uses
+app.use( express.static( 'server/public' ) );
+
+// globals
+const port = 5000;
+
+// spin up server
+app.listen( port, () => {
+    console.log( 'server is up on', port );
+})
+
+// routes
